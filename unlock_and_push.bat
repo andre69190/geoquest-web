2eed8b7c2484980d28a00eb9d67f71a513546459
Andre <andre69190@googlemail.com>
2026-05-23 01:00:15 +0200

    Feature: Favorites with full i18n (14 languages), heart per card, showFavorites filter

--- unlock_and_push.bat
+++ unlock_and_push.bat
@@ -6,7 +6,7 @@ if exist ".git\HEAD.lock" del /f /q ".git\HEAD.lock"
 if exist ".git\refs\heads\main.lock" del /f /q ".git\refs\heads\main.lock"
 echo.
 git add -A
-git commit -m "Feature: Favorites - heart button per card, showFavorites() filter, localStorage persist"
+git commit -m "Feature: Favorites with full i18n (14 languages), heart per card, showFavorites filter"
 echo.
 git push origin main
 echo.
